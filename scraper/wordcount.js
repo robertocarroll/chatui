82187348980119b863bbbcc1afab344406e2d88a
robertocarroll <robertocarroll@gmail.com>
2014-10-08 15:57:05 +0100

    Notes about scraping in the readme and tidy up the package details. Fixes issue #5

--- scraper/wordcount.js
+++ scraper/wordcount.js
@@ -18,14 +18,11 @@ function countWordsInText(text) {
     }
 }
 
-
-
 fs.readFile('data/articles.json', 'utf8', function (err, topicJSON) {
   var articleArray = [], articles = JSON.parse(topicJSON);
 
   articles.forEach(function(item, index) {
       articleArray.push({article: item.article});
-
       
   });
 
