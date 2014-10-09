dc7ce968efcdfa63982041340e19893e72f3a4c7
robertocarroll <robertocarroll@gmail.com>
2014-10-09 10:42:05 +0100

    Cleaned up wordcount code a bit

--- scraper/wordcount.js
+++ scraper/wordcount.js
@@ -1,6 +1,31 @@
 fs = require("fs"),
 	commaIt = require('comma-it');
 
+fs.readFile('data/articles.json', 'utf8', function (err, topicJSON) {
+  var articleArray = [], articles = JSON.parse(topicJSON);
+
+  articles.forEach(function(item, index) {
+      articleArray.push({title: item.title, date: item.date, link: item.link, article: item.article});      
+  });
+
+  var text = {};
+  var arrayLength = articleArray.length
+		for (var i=0; i<arrayLength; ++i) {
+
+		// example here: http://stackoverflow.com/questions/750486/javascript-closure-inside-loops-simple-practical-example	
+
+		(function(index) {
+        var s = articleArray[i].article;				   
+				text = s.toString();
+				countWordsInText(text);
+    })(i);
+
+		}
+
+		totalCount();
+
+});
+
 var wordCounts = Object.create(null);
 
 function countWordsInText(text) {
@@ -16,39 +41,26 @@ function countWordsInText(text) {
             wordCounts[word] = (wordCounts[word]) ? wordCounts[word] + 1 : 1;
         }
     }
+
 }
 
-fs.readFile('data/articles.json', 'utf8', function (err, topicJSON) {
-  var articleArray = [], articles = JSON.parse(topicJSON);
-
-  articles.forEach(function(item, index) {
-      articleArray.push({article: item.article});      
-  });
-
-   var text = {};
-  var arrayLength = articleArray.length
-		for (var i=0; i<arrayLength; ++i) {
-		  if (i in articleArray) {
-		    var s = articleArray[i].article;				   
-				text = s.toString();
-				countWordsInText(text);
-
-		  }
-		}
-
-	var totalWords = 0;
-		
+function totalCount () {
+		var totalWords = 0;
 		for (var key in wordCounts) {
 		    totalWords += wordCounts[key];
 		};
 
 	var readingTime = Math.round(totalWords / 250);	
-
 	totalWords = commaIt(totalWords, {precision: 0, thousandSeperator : ','});
 
-	 console.log('The total number of words is ' + totalWords 
+	 console.log('The total number of words MEN have written about me is ' + totalWords 
 	 	+ ' and the average reader would take ' + readingTime + ' minutes to read them' );
 
-});
+}	 
+
+
+
+
+
 
 
